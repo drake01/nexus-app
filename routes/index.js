8483239df8d26b62008c2d300b194374abbfbac6
Gulshan Kumar <drakeoone@gmail.com>
2021-06-16 19:45:01 +0530

    base /greet route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/greet', function(req, res, next) {
+  out = 'Got a GET request at /greet\nGreetings Friend!!\n';
+  const greet = process.env.GREET || 'DEFAULT VALUE';
+  out += `Using environment variable $GREET: ${greet} \n`;
+  res.send(out);
+});
+
 module.exports = router;
